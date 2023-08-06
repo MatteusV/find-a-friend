bee912f751d8735878ec2b03c7f31d12432f6fe9
Matteus Varlesse <varlesse04@gmail.com>
2023-08-06 13:10:03 -0300

    feat: changing input select to text input and ending home page on desktop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,15 @@ import { Logo } from '@/components/icons/logo'
 import dogs from '@/assets/dogs.png'
 import Image from 'next/image'
 import { Search } from '@/components/icons/search'
+import { useForm } from 'react-hook-form'
+
 export default function Home() {
+  const { register, handleSubmit } = useForm()
+
+  function handleLocal(data) {
+    console.log(data)
+  }
+
   return (
     <div className="w-full h-screen  max-sm:p-4 text-white md:flex md:flex-col md:justify-around md:gap-10 md:px-[7.06rem] md:py-[7.75rem]">
       <header className="w-full flex max-sm:justify-center max-sm:-ml-3 max-sm:mt-2">
@@ -17,32 +25,51 @@ export default function Home() {
         <Image src={dogs} alt="" className="max-sm:mt-10" />
       </main>
 
-      <footer className="border max-sm:mt-10 flex justify-between max-sm:flex-col">
+      <footer className="max-sm:mt-10 flex justify-between max-sm:flex-col">
         <h3 className="max-sm:text-justify text-lg ">
-          Encontre o animal de estimação ideal para seu estilo de vida!
+          Encontre o animal de estimação ideal <br className="max-sm:hidden" />
+          para seu estilo de vida!
         </h3>
 
-        <div className="max-sm:w-full flex items-center md:gap-5 max-sm:flex-col max-sm:gap-5">
-          <p className="max-sm:mt-10 ">Busque um amigo:</p>
-          <select className="bg-background border rounded-lg md:rounded-xl max-sm:p-1 max-sm:pl-4 md:text-center md:p-3">
+        <form
+          onSubmit={handleSubmit(handleLocal)}
+          className="max-sm:w-full flex items-center max-sm:flex-col max-sm:items-center max-sm:gap-5  md:gap-3"
+        >
+          <p className="max-sm:mt-10">Busque um amigo:</p>
+          <input
+            type="text"
+            list="countryList"
+            {...register('country')}
+            placeholder="Estado"
+            className="bg-background border placeholder:text-white rounded-lg  md:w-[100px] max-sm:p-1 max-sm:pl-4 md:text-center md:p-3"
+          />
+          <datalist id="countryList">
             <option value="PE">PE</option>
-            <option value="PE">SP</option>
-            <option value="PE">RJ</option>
-          </select>
+            <option value="SP">SP</option>
+            <option value="RJ">RJ</option>
+          </datalist>
 
-          <select className="bg-[#E44449] rounded-lg max-sm:p-2 max-sm:pl-4 md:text-center md:p-3">
-            <option value="PE">Recife</option>
-            <option value="PE">São José dos Campos</option>
-            <option value="PE">Rio de Janeiro</option>
-          </select>
+          <input
+            {...register('city')}
+            type="text"
+            list="cityList"
+            placeholder="Cidade"
+            className="bg-[#E44449] placeholder:text-white rounded-lg max-sm:p-2 max-sm:pl-4 md:text-center md:p-3"
+          />
+
+          <datalist id="cityList">
+            <option value="Recife">Recife</option>
+            <option value="São José dos Campos">São José dos Campos</option>
+            <option value="Rio de Janeiro">Rio de Janeiro</option>
+          </datalist>
 
           <button
             type="submit"
-            className="max-sm:w-[150px] max-sm:ml-12 max-sm:rounded-lg bg-[#F4D35E] max-sm:flex max-sm:justify-center max-sm:p-2 md:p-3 md:rounded-xl hover:cursor-pointer hover:bg-[#ffd22d]"
+            className="max-sm:w-[150px] max-sm:rounded-lg bg-[#F4D35E] max-sm:flex max-sm:justify-center max-sm:p-2 md:p-3 md:rounded-xl hover:cursor-pointer hover:bg-[#ffd22d]"
           >
             <Search />
           </button>
-        </div>
+        </form>
       </footer>
     </div>
   )
---
+++ src/pages/map/[...local].tsx
@@ -0,0 +1,10 @@
+export default function Map() {
+  return (
+    <div>
+      <aside>
+        <h1>aside</h1>
+      </aside>
+      <main></main>
+    </div>
+  )
+}
